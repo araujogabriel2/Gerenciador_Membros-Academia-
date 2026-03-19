dd4d1112b774afa5470a5705c05fdf418dcd5699
araujogabriel2 <ga224229@gmail.com>
2026-03-19 09:04:46 -0300

    feat: cria funções de registrar e listar membros

--- app/models.py
+++ app/models.py
@@ -12,4 +12,29 @@ def criar_tabela():
                     plano TEXT NOT NULL,
                     ativo BOOLEAN NOT NULL DEFAULT 1
                 )
-            """)    
+            """)
+    
+def registrar_membro(socios):
+    conn = conectar()
+    cursor = conn.cursor()
+
+    cursor.execute("INSERT INTO socios (nome, idade, plano, ativo) VALUES(?,?,?,?)", (
+        socios.nome, socios.idade, socios.plano, socios.ativo
+    ))
+    conn.commit()
+    novo_id = cursor.lastrowid
+    conn.close()
+
+    return {
+        "mensagem": f"Membro {socios.nome} cadastrado com sucesso!",
+        "ID": novo_id
+    }
+
+def listar_membros():
+    conn = conectar()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM socios")
+    conn.close()
+    dados = cursor.fetchall()
+    return dados
